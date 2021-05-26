1f61bc3f35d0939a742e53d45029446aeae0583a
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-05-26 17:23:59 +0800

    counter

---
+++ src/counter.erl
@@ -0,0 +1,54 @@
+%%%-------------------------------------------------------------------
+%%% @author dannypsnl
+%%% @copyright (C) 2021, <COMPANY>
+%%% @doc
+%%% @end
+%%%-------------------------------------------------------------------
+-module(counter).
+
+-behaviour(gen_server).
+
+-export([start_link/1]).
+-export([
+    init/1,
+    handle_call/3,
+    handle_cast/2,
+    handle_info/2,
+    terminate/2,
+    code_change/3
+]).
+
+-define(SERVER, ?MODULE).
+
+-record(counter_state, {count}).
+
+%%%===================================================================
+%%% Spawning and gen_server implementation
+%%%===================================================================
+
+start_link(Val) ->
+    gen_server:start_link({local, ?SERVER}, ?MODULE, Val, []).
+
+init(Val) ->
+    {ok, #counter_state{count = Val}}.
+
+handle_call(next, _From, State = #counter_state{count = Val}) ->
+    {reply, Val, State#counter_state{count = Val + 1}};
+handle_call(current, _From, State = #counter_state{count = Val}) ->
+    {reply, Val, State}.
+
+handle_cast(_Request, State) ->
+    {noreply, State}.
+
+handle_info(_Info, State) ->
+    {noreply, State}.
+
+terminate(_Reason, _State) ->
+    ok.
+
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
